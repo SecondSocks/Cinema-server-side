706128a5f11f4e1052952dee8624534861823b33
SecondSocks <159525526+SecondSocks@users.noreply.github.com>
2025-02-19 21:21:10 +0700

    Auth was completed

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,6 @@
-import { BadRequestException, Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { verify } from 'argon2'
 import { PrismaService } from 'src/prisma.service'
 import { UserService } from 'src/user/user.service'
 import { AuthDto } from './dto/auth.dto'
@@ -7,16 +9,85 @@ import { AuthDto } from './dto/auth.dto'
 export class AuthService {
 	constructor(
 		private readonly prismaService: PrismaService,
-		private readonly userService: UserService
+		private readonly userService: UserService,
+		private readonly jwtService: JwtService
 	) {}
 
 	async register(dto: AuthDto) {
+		if (dto.email === '' || dto.password === '') throw new BadRequestException('Request must contain email and password')
+
 		const oldUser = await this.prismaService.user.findUnique({
 			where: { email: dto.email }
 		})
 
 		if (oldUser) throw new BadRequestException('User with this email already exists')
 
-		return this.userService.create(dto)
+		const user = await this.userService.create(dto)
+		const tokens = await this.issueTokens(user.id)
+
+		return {
+			user: this.userService.returnUserField(user),
+			accessToken: tokens.accessToken,
+			refreshToken: tokens.refreshToken
+		}
+	}
+
+	async login(dto: AuthDto) {
+		if (dto.email === '' || dto.password === '') throw new BadRequestException('Request must contain email and password')
+
+		const user = await this.validateUser(dto)
+		const tokens = await this.issueTokens(user.id)
+
+		return {
+			user: this.userService.returnUserField(user),
+			accessToken: tokens.accessToken,
+			refreshToken: tokens.refreshToken
+		}
+	}
+
+	async getNewTokens(refreshToken: string) {
+		const result = await this.jwtService.verifyAsync(refreshToken)
+
+		if (!result) throw new UnauthorizedException('Invalid token')
+
+		const user = await this.userService.getById(result.id)
+		const tokens = await this.issueTokens(user.id)
+
+		return {
+			user: this.userService.returnUserField(user),
+			accessToken: tokens.accessToken,
+			refreshToken: tokens.refreshToken
+		}
+	}
+
+	private async issueTokens(userId: string) {
+		const data = {
+			id: userId
+		}
+
+		const accessToken = this.jwtService.sign(data, {
+			expiresIn: '1h'
+		})
+
+		const refreshToken = this.jwtService.sign(data, {
+			expiresIn: '7d'
+		})
+
+		return {
+			accessToken,
+			refreshToken
+		}
+	}
+
+	private async validateUser(dto: AuthDto) {
+		const user = await this.userService.getByEmail(dto.email)
+
+		if (!user) throw new BadRequestException('User with this email does not exist')
+
+		const isValid = await verify(user.password, dto.password)
+
+		if (!isValid) throw new UnauthorizedException('Invalid password')
+
+		return user
 	}
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common'
+import { User } from '@prisma/client'
 import { hash } from 'argon2'
 import { AuthDto } from 'src/auth/dto/auth.dto'
 import { PrismaService } from 'src/prisma.service'
@@ -12,7 +13,30 @@ export class UserService {
 			data: {
 				email: dto.email,
 				password: await hash(dto.password),
-				name: 'John Doe'
+				name: 'No name'
+			}
+		})
+	}
+
+	async returnUserField(user: User) {
+		return {
+			id: user.id,
+			email: user.email
+		}
+	}
+
+	async getById(userId: string) {
+		return this.prismaService.user.findUnique({
+			where: {
+				id: userId
+			}
+		})
+	}
+
+	async getByEmail(email: string) {
+		return this.prismaService.user.findUnique({
+			where: {
+				email
 			}
 		})
 	}
